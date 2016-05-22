17e237f51e854b1435e0ea3dc6dc0c9d17b34f23
Aaron Heath <aaron@aaronheath.com>
2016-05-23 06:41:43 +0930

    Progress made on MatchesService unit tests.

--- src/app/tests/example-data-matches.ts
+++ src/app/tests/example-data-matches.ts
@@ -1,59 +1,241 @@
 declare const moment;
 
-const matches : IMatch[] = [
-    {
-        h_home: 'Essendon',
-        result: 'ESS',
-        home: 'ESS',
-        homeGoals: 15,
-        homeBehinds: 10,
-        homePoints: 100,
-        away: 'ADL',
-        awayGoals: 14,
-        awayBehinds: 11,
-        awayPoints: 95,
-        venue: 'MCG',
-        h_venue: 'Melbourne Cricket Ground',
-        attendance: 56482,
-        h_home_abbr: 'ESS',
-        h_away_abbr: 'ADL',
-        date: '26/7',
-        time: '13:15',
-    },
-    {
-        h_home: 'Port Adelaide',
-        result: 'PA',
-        home: 'PA',
-        homeGoals: 19,
-        homeBehinds: 13,
-        homePoints: 127,
-        away: 'COLL',
-        awayGoals: 3,
-        awayBehinds: 21,
-        awayPoints: 39,
-        venue: 'AO',
-        h_venue: 'Adelaide Oval',
-        attendance: 34561,
-        h_home_abbr: 'PA',
-        h_away_abbr: 'COLL',
-        date: '27/7',
-        time: '14:40',
-    },
-];
-
-const venueTimezones = {
-    AO: 'Australia/Adelaide',
-    MCG: 'Australia/Melbourne',
-};
+import {loopMatches} from '../helpers/matches';
+import {getVenues} from './example-data-venues';
 
 const timezone = 'Australia/Adelaide';
 
-function _assignTimes() {
-    return matches.map((match, i) => {
+const matches = {
+    1: [
+        {
+            home: 'RICH',
+            homeGoals: 14,
+            homeBehinds: 8,
+            away: 'CARL',
+            awayGoals: 12,
+            awayBehinds: 11,
+            venue: 'MCG',
+            date: '24/3',
+            time: '19:20',
+            attendance: 75706,
+        },
+        {
+            home: 'MEL',
+            homeGoals: 12,
+            homeBehinds: 8,
+            away: 'GWS',
+            awayGoals: 10,
+            awayBehinds: 18,
+            venue: 'MCG',
+            date: '26/3',
+            time: '13:40',
+            attendance: 28505,
+        },
+        {
+            home: 'GC',
+            homeGoals: 17,
+            homeBehinds: 19,
+            away: 'ESS',
+            awayGoals: 9,
+            awayBehinds: 6,
+            venue: 'MS',
+            date: '26/3',
+            time: '15:35',
+            attendance: 16239,
+        },
+        {
+            home: 'NM',
+            homeGoals: 16,
+            homeBehinds: 11,
+            away: 'ADL',
+            awayGoals: 14,
+            awayBehinds: 13,
+            venue: 'ES',
+            date: '26/3',
+            time: '19:25',
+            attendance: 25485,
+        },
+        {
+            home: 'SYD',
+            homeGoals: 18,
+            homeBehinds: 25,
+            away: 'COLL',
+            awayGoals: 7,
+            awayBehinds: 11,
+            venue: 'SCG',
+            date: '26/3',
+            time: '19:25',
+            attendance: 33857,
+        },
+        {
+            home: 'WB',
+            homeGoals: 15,
+            homeBehinds: 13,
+            away: 'FRE',
+            awayGoals: 5,
+            awayBehinds: 8,
+            venue: 'ES',
+            date: '27/3',
+            time: '13:10',
+            attendance: 27832,
+        },
+        {
+            home: 'PA',
+            homeGoals: 20,
+            homeBehinds: 13,
+            away: 'STK',
+            awayGoals: 15,
+            awayBehinds: 10,
+            venue: 'AO',
+            date: '27/3',
+            time: '14:50',
+            attendance: 43807,
+        },
+        {
+            home: 'WC',
+            homeGoals: 26,
+            homeBehinds: 10,
+            away: 'BL',
+            awayGoals: 15,
+            awayBehinds: 12,
+            venue: 'DS',
+            date: '27/3',
+            time: '16:40',
+            attendance: 35201,
+        },
+        {
+            home: 'GEEL',
+            homeGoals: 18,
+            homeBehinds: 8,
+            away: 'HAW',
+            awayGoals: 12,
+            awayBehinds: 14,
+            venue: 'MCG',
+            date: '28/3',
+            time: '15:20',
+            attendance: 74218,
+        },
+    ],
+    2: [
+        {
+            home: 'COLL',
+            homeGoals: 13,
+            homeBehinds: 9,
+            away: 'RICH',
+            awayGoals: 12,
+            awayBehinds: 14,
+            venue: 'MCG',
+            date: '1/4',
+            time: '19:50',
+            attendance: 72761,
+        },
+        {
+            home: 'ADL',
+            homeGoals: 22,
+            homeBehinds: 12,
+            away: 'PA',
+            awayGoals: 11,
+            awayBehinds: 20,
+            venue: 'AO',
+            date: '2/4',
+            time: '13:15',
+            attendance: 51585,
+        },
+        {
+            home: 'ESS',
+            //homeGoals: 11,
+            //homeBehinds: 14,
+            away: 'MEL',
+            //awayGoals: 10,
+            //awayBehinds: 7,
+            venue: 'MCG',
+            date: '2/4',
+            time: '14:10',
+            //attendance: 50424,
+        },
+        {
+            home: 'BL',
+            //homeGoals: 11,
+            //homeBehinds: 17,
+            away: 'NM',
+            //awayGoals: 17,
+            //awayBehinds: 15,
+            venue: 'G',
+            date: '2/4',
+            time: '15:35',
+            //attendance: 18021,
+        },
+        {
+            home: 'STK',
+            //homeGoals: 5,
+            //homeBehinds: 6,
+            away: 'WB',
+            //awayGoals: 13,
+            //awayBehinds: 15,
+            venue: 'ES',
+            date: '2/4',
+            time: '19:25',
+            //attendance: 37353,
+        },
+        {
+            home: 'FRE',
+            //homeGoals: 14,
+            //homeBehinds: 16,
+            away: 'GC',
+            //awayGoals: 19,
+            //awayBehinds: 12,
+            venue: 'DS',
+            date: '2/4',
+            time: '16:40',
+            //attendance: 34208,
+        },
+        {
+            home: 'GWS',
+            //homeGoals: 13,
+            //homeBehinds: 11,
+            away: 'GEEL',
+            //awayGoals: 11,
+            //awayBehinds: 10,
+            venue: 'MO',
+            date: '3/4',
+            time: '13:10',
+            //attendance: 13656,
+        },
+        {
+            home: 'HAW',
+            //homeGoals: 14,
+            //homeBehinds: 15,
+            away: 'WC',
+            //awayGoals: 7,
+            //awayBehinds: 11,
+            venue: 'MCG',
+            date: '3/4',
+            time: '15:20',
+            //attendance: 42977,
+        },
+        {
+            home: 'CARL',
+            //homeGoals: 10,
+            //homeBehinds: 11,
+            away: 'SYD',
+            //awayGoals: 20,
+            //awayBehinds: 11,
+            venue: 'ES',
+            date: '3/4',
+            time: '16:40',
+            //attendance: 33146,
+        },
+    ],
+};
+
+function _assignTimes(_matches) {
+    const venues = getVenues();
+
+    return loopMatches(_matches, (match) => {
         match.venue_moment = moment.tz(
             `${match.date}/2016 ${match.time}`,
             'D/M/YYYY HH:mm',
-            venueTimezones[match.venue]
+            venues[match.venue].timezone
         );
 
         match.h_date = match.venue_moment.format('ddd D MMM');
@@ -69,9 +251,53 @@ function _assignTimes() {
 }
 
 function getMatches() {
-    return _assignTimes();
+    return matches;
+}
+
+function getMatchesWithTimes(_matches?) {
+    if(!_matches) {
+        _matches = matches;
+    }
+
+    return _assignTimes(_matches);
+}
+
+function getMatchesWithPoints(_matches?) {
+    if(!_matches) {
+        _matches = matches;
+    }
+
+    return loopMatches(_matches, (match) => {
+        if(match.homeGoals && match.homeBehinds && match.awayGoals && match.awayBehinds) {
+            match.homePoints = match.homeGoals * 6 + match.homeBehinds;
+            match.awayPoints = match.awayGoals * 6 + match.awayBehinds;
+
+            match.margin = Math.abs(match.homePoints - match.awayPoints);
+
+            if(match.homePoints === match.awayPoints) {
+                match.result = 'DRAW';
+            } else {
+                match.result = (match.homePoints > match.awayPoints) ? match.home : match.away;
+            }
+        }
+
+        return match;
+    });
+}
+
+function getMatchesWithPointsAndTimes(_matches?) {
+    if(!_matches) {
+        _matches = matches;
+    }
+
+    const matchesWithTimes = getMatchesWithTimes(_matches);
+
+    return getMatchesWithPoints(matchesWithTimes);
 }
 
 export {
     getMatches,
+    getMatchesWithTimes,
+    getMatchesWithPoints,
+    getMatchesWithPointsAndTimes,
 };
---
+++ src/app/tests/example-data-venues.ts
@@ -0,0 +1,115 @@
+const venues = {
+    MCG: {
+        fullName: 'Melbourne Cricket Ground',
+        abbreviation: 'MCG',
+        city: 'Melbourne',
+        state: 'VIC',
+        timezone: 'Australia/Melbourne',
+    },
+    MS: {
+        fullName: 'Metricon Stadium',
+        abbreviation: 'MS',
+        city: 'Gold Coast',
+        state: 'QLD',
+        timezone: 'Australia/Brisbane',
+    },
+    ES: {
+        fullName: 'Ethiad Stadium',
+        abbreviation: 'ES',
+        city: 'Melbourne',
+        state: 'VIC',
+        timezone: 'Australia/Melbourne',
+    },
+    SCG: {
+        fullName: 'Sydney Cricket Ground',
+        abbreviation: 'SCG',
+        city: 'Sydney',
+        state: 'NSW',
+        timezone: 'Australia/Sydney',
+    },
+    AO: {
+        fullName: 'Adelaide Oval',
+        abbreviation: 'AO',
+        city: 'Adelaide',
+        state: 'SA',
+        timezone: 'Australia/Adelaide',
+    },
+    DS: {
+        fullName: 'Domain Stadium',
+        abbreviation: 'DS',
+        city: 'Perth',
+        state: 'WA',
+        timezone: 'Australia/Perth',
+    },
+    AU: {
+        fullName: 'Aurora Stadium',
+        abbreviation: 'AS',
+        city: 'Launceston',
+        state: 'TAS',
+        timezone: 'Australia/Hobart',
+    },
+    BA: {
+        fullName: 'Blundstone Arena',
+        abbreviation: 'BA',
+        city: 'Hobart',
+        state: 'TAS',
+        timezone: 'Australia/Hobart',
+    },
+    CS: {
+        fullName: 'Cazaly\'s Stadium',
+        abbreviation: 'CS',
+        city: 'Cairns',
+        state: 'QLD',
+        timezone: 'Australia/Brisbane',
+    },
+    MO: {
+        fullName: 'Manuka Oval',
+        abbreviation: 'MO',
+        city: 'Canberra',
+        state: 'ACT',
+        timezone: 'Australia/Sydney',
+    },
+    SPO: {
+        fullName: 'Spotless Stadium',
+        abbreviation: 'SPO',
+        city: 'Sydney',
+        state: 'NSW',
+        timezone: 'Australia/Sydney',
+    },
+    SS: {
+        fullName: 'Simonds Stadium',
+        abbreviation: 'SS',
+        city: 'Geelong',
+        state: 'VIC',
+        timezone: 'Australia/Melbourne',
+    },
+    TIO: {
+        fullName: 'TIO Stadium',
+        abbreviation: 'TIO',
+        city: 'Darwin',
+        state: 'NT',
+        timezone: 'Australia/Darwin',
+    },
+    TP: {
+        fullName: 'TIO Traeger Park',
+        abbreviation: 'TP',
+        city: 'Alice Springs',
+        state: 'NT',
+        timezone: 'Australia/Darwin',
+    },
+    G: {
+        fullName: 'Gabba',
+        abbreviation: 'G',
+        city: 'Brisbane',
+        state: 'QLD',
+        timezone: 'Australia/Brisbane',
+    },
+};
+
+function getVenues() {
+    return venues;
+}
+
+export {
+    getVenues
+};
