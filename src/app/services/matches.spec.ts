1d396e6281e89a3d3853319ba14913cbaba07db0
Aaron Heath <aaron@aaronheath.com>
2016-05-26 06:37:31 +0930

    MatchesService unit test now checking http calls on load() method.

--- src/app/services/matches.spec.ts
+++ src/app/services/matches.spec.ts
@@ -20,7 +20,10 @@ import {Observable} from 'rxjs/Observable';
 import {getMatches, getMatchesWithPointsAndTimes} from '../tests/example-data-matches';
 
 describe('MatchesService', () => {
+    const standardTimeout = 1000; // 1 second
     const matchesExpected = getMatchesWithPointsAndTimes();
+
+    let mockedBackend;
 
     beforeEachProviders(() => [
         provide(
@@ -42,46 +45,77 @@ describe('MatchesService', () => {
             Http,
             {
                 useFactory: (backend, options) => {
-                    return new Http(backend, options);
+                    const _http = new Http(backend, options);
+
+                    spyOn(_http, 'get').and.callThrough();
+
+                    return _http;
                 },
                 deps: [MockBackend, BaseRequestOptions],
             }
         ),
     ]);
 
-    beforeEach(inject([MockBackend], (_mockbackend) => {
+    beforeEach(inject([MockBackend, Http], (_mockbackend, _http) => {
         const baseResponse = new Response(new ResponseOptions({body: getMatches()}));
 
         _mockbackend.connections.subscribe((c:MockConnection) => c.mockRespond(baseResponse));
+
+        mockedBackend = _mockbackend;
     }));
 
-    it('should be constructed', inject([MatchesService], (matchesService: MatchesService) => {
+    it('should be constructed', inject([MatchesService, Http], (matchesService: MatchesService, http: Http) => {
         matchesService.observable$.subscribe((data) => {
             expect(matchesService.load).toHaveBeenCalled();
             expect(matchesService.observable$).toEqual(jasmine.any(Observable));
         });
     }));
 
-    it('should parse and add calculated attr to matches', inject([MatchesService], (matchesService: MatchesService) => {
-        matchesService.observable$.subscribe((data) => {
-            const allMatches = matchesService.getAllMatches();
+    it('load() should fetch match data, add match attrs and call next on the observer', inject([
+        MatchesService
+    ], (matchesService: MatchesService) => {
+        const promise = new Promise((resolve, reject) => {
+            let loadCalls = 0;
 
-            // Round 1 Match 1
-            const receivedRound1Match1 = allMatches[1][0];
-            const expectedRound1Match1 = matchesExpected[1][0];
-            compareMatches(receivedRound1Match1, expectedRound1Match1);
+            matchesService.observable$.subscribe((data) => {
+                loadCalls++;
 
-            // Round 2 Match 2
-            const receivedRound2Match2 = allMatches[2][1];
-            const expectedRound2Match2 = matchesExpected[2][1];
-            compareMatches(receivedRound2Match2, expectedRound2Match2);
+                if(loadCalls === 1) {
+                    matchesService.load();
+                }
 
-            // Round 2 Match 3 (Match not played yet)
-            const receivedRound2Match3 = allMatches[2][2];
-            const expectedRound2Match3 = matchesExpected[2][2];
-            compareMatches(receivedRound2Match3, expectedRound2Match3);
+                if(loadCalls === 2) {
+                    expect(matchesService.load).toHaveBeenCalledTimes(loadCalls);
+
+                    expect(mockedBackend.connectionsArray.length).toBe(2);
+                    expect(mockedBackend.connectionsArray[1].request.method).toBe(0); // GET
+                    expect(mockedBackend.connectionsArray[1].request.url).toBe('/data/matches.json');
+
+                    resolve();
+                }
+
+                //const allMatches = matchesService.getAllMatches();
+                //
+                //// Round 1 Match 1
+                //const receivedRound1Match1 = allMatches[1][0];
+                //const expectedRound1Match1 = matchesExpected[1][0];
+                //compareMatches(receivedRound1Match1, expectedRound1Match1);
+                //
+                //// Round 2 Match 2
+                //const receivedRound2Match2 = allMatches[2][1];
+                //const expectedRound2Match2 = matchesExpected[2][1];
+                //compareMatches(receivedRound2Match2, expectedRound2Match2);
+                //
+                //// Round 2 Match 3 (Match not played yet)
+                //const receivedRound2Match3 = allMatches[2][2];
+                //const expectedRound2Match3 = matchesExpected[2][2];
+                //compareMatches(receivedRound2Match3, expectedRound2Match3);
+            });
         });
-    }));
+
+        return promise;
+    }),
+    standardTimeout);
 });
 
 function compareMatches(received, expected) {
