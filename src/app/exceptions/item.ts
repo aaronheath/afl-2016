60f1065f14ec74cddae9b8e3d6a6fd627fcb8ab3
Aaron Heath <aaron@aaronheath.com>
2016-06-28 03:19:00 +0930

    Commented exceptions.

--- src/app/exceptions/item.ts
+++ src/app/exceptions/item.ts
@@ -1,5 +1,8 @@
-import {BaseException} from './base';
+import { BaseException } from './index';
 
+/**
+ * Extends BaseException for use with the base models Item class.
+ */
 export class ItemException extends BaseException {
     constructor(message : string) {
         super(message);
--- src/app/exceptions/model.ts
+++ src/app/exceptions/model.ts
@@ -1,5 +1,8 @@
-import {BaseException} from './base';
+import { BaseException } from './index';
 
+/**
+ * Extends BaseException for use with the base Model class.
+ */
 export class ModelException extends BaseException {
     constructor(message : string) {
         super(message);
